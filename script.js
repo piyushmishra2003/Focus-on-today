ed87095bb8590fad54b15dca8146ccf29e95adce
piyushmishra2003 <mishrapiyush5085@gmail.com>
2025-07-31 17:14:49 +0530

    This is my first

---
+++ script.js
@@ -0,0 +1,30 @@
+const checkBoxList = document.querySelectorAll('.custom-checkbox');
+const inputFields = document.querySelectorAll('.goal-input');
+const errorLabel = document.querySelector('.error-label');
+const progressBar = document.querySelector('.progress-bar');
+const progressValue=document.querySelector('.progress-value');
+
+checkBoxList.forEach((checkbox) => {
+    checkbox.addEventListener('click', (e) => {
+        const allGoalsAdded = [...inputFields].every(function (input) {
+            // console.log(input.value);
+            return input.value
+        })
+
+        if (allGoalsAdded) {
+            checkbox.parentElement.classList.toggle('completed')
+            progressValue.style.width='33.3%'
+        }
+        else {
+            progressBar.classList.add('show-error')
+            // errorLabel.style.display="none"
+        }
+    })
+});
+
+inputFields.forEach((input) => {
+    input.addEventListener('focus', () => {
+        progressBar.classList.remove('show-error')
+    })
+})
+
